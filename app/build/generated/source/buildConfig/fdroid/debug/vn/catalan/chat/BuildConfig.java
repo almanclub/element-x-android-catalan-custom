5ea33badf5df7472f49dc5de4404664a17b7cb81
Alman <54709085+almanclub@users.noreply.github.com>
2025-06-26 17:33:31 +0700

    Add files via upload

---
+++ app/build/generated/source/buildConfig/fdroid/debug/vn/catalan/chat/BuildConfig.java
@@ -0,0 +1,21 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package vn.catalan.chat;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "vn.catalan.chat.debug";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "fdroid";
+  public static final int VERSION_CODE = 202506030;
+  public static final String VERSION_NAME = "25.06.3";
+  // Field from product flavor: fdroid
+  public static final String FLAVOR_DESCRIPTION = "FDroid";
+  // Field from default config.
+  public static final String GIT_BRANCH_NAME = "master";
+  // Field from default config.
+  public static final String GIT_REVISION = "9e6aaee4";
+  // Field from product flavor: fdroid
+  public static final String SHORT_FLAVOR_DESCRIPTION = "F";
+}
